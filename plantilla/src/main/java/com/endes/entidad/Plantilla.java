05bebd8872a09ef87a384b23a0c14c565ff1e3e7
Sergio Campos Delgado <campos.delgado.sergio@iescamas.es>
2025-03-20 10:26:32 +0100

    Metodo refactorizado

--- plantilla/src/main/java/com/endes/entidad/Plantilla.java
+++ plantilla/src/main/java/com/endes/entidad/Plantilla.java
@@ -27,6 +27,17 @@ public class Plantilla {
         if (empleado == null) {
             throw new IllegalArgumentException("No se puede contratar un empleado nulo");
         }
+        
+        boolean repetido = false;
+        for (int i = 0; i < empleados.size()  || repetido; i++) {
+        	if (empleados.get(i).getDni().equals(empleado.getDni())) {
+        		repetido = true;
+        	}
+        }
+        
+        if (repetido) {
+        	throw new IllegalArgumentException("No se puede contratar un empleado repetido");
+        }
         empleados.add(empleado);
     }
 
